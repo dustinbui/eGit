48cc897abd66391a9945ef7b98dbb2d082b8bffc
dustinbui <vietanh98bn1998@gmail.com>
2018-07-23 20:37:46 +0700

    GHI

--- TestEGit/src/Test.java
+++ TestEGit/src/Test.java
@@ -3,5 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("ABC");
 		System.out.println("DEF");
+		System.out.println("GHI");
+		System.out.println("JJJ");
 	}
 }
